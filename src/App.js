3a7e06fe8dfb7d351f8f7e4908a25c55dac17fa7
num4yo3 <82578883+num4yo3@users.noreply.github.com>
2021-12-20 18:44:52 +0900

    スタイルを調整

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ export const App = () => {
   return (
     <ul className="menu">
       <div className="menu_title">menu</div>
-      <li class="menu_item">first</li>
-      <li class="menu_item">second</li>
-      <li class="menu_item">third</li>
-      <li class="menu_item">fourth</li>
+      <a href="#">
+        <li class="menu_item">first</li>
+      </a>
+      <a href="#">
+        <li class="menu_item">second</li>
+      </a>
+      <a href="#">
+        <li class="menu_item">third</li>
+      </a>
+      <a href="#">
+        <li class="menu_item">fourth</li>
+      </a>
     </ul>
   );
 };
