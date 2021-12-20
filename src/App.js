8bdbe14a73de47cc374210f79eb0d1e6d489967c
num4yo3 <82578883+num4yo3@users.noreply.github.com>
2021-12-20 18:33:03 +0900

    元となるhtmlファイルを作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./styles.css";
 
 export const App = () => {
-  return <h1>Hello!</h1>;
+  return (
+    <ul className="menu">
+      <div className="menu_title">menu</div>
+      <li class="menu_item">first</li>
+      <li class="menu_item">second</li>
+      <li class="menu_item">third</li>
+      <li class="menu_item">fourth</li>
+    </ul>
+  );
 };
